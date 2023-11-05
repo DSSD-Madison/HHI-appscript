5c3e36378be069a5746d8940d2cd4bdeb3a73a31
GeoffreyX <xuegeo21@pewaukeeschools.org>
2023-11-05 14:14:46 -0600

    replace placeholder function with onEdit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { onStakeholderUpdateEmail } from "./email";
 import { onStakeholderApprovalGeocode } from "./geocode";
 
-// A placeholder function to list all available hooks wtihin the project
-function allHooks() {
-  onStakeholderUpdateEmail(null)
-  onStakeholderApprovalGeocode(null)
+function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
+  onStakeholderUpdateEmail(e)
+  onStakeholderApprovalGeocode(e)
 }
 
