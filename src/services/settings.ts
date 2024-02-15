fd6f446e0fef478e37d6ca8a9c77d6ef7bddf5b8
GeoffreyXue <xuegeoff@gmail.com>
2024-02-15 17:59:49 -0600

    Add debug to settings

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,7 +1,8 @@
-import { SETTING_SHEET_ID } from "../constants";
+import { DEBUG, SETTING_SHEET_ID } from "../constants";
 
 
 export function getSetting(a1Notation: string): string {
+  if (DEBUG) Logger.log(`Retriving setting at ${a1Notation}`)
   return SpreadsheetApp.getActive()
     .getSheets()
     .find((s) => s.getSheetId() === SETTING_SHEET_ID)
